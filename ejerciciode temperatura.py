8fa5b2f0c7d38b7dfabffa4213aafb91dc957358
dixongonzalez <dixonandresgonzalez2010@gmail.com>
2025-07-21 12:03:37 -0400

    ejercicio de temperatura.py

--- ejerciciode temperatura.py
+++ ejerciciode temperatura.py
@@ -1,5 +1,5 @@
 Temperatura = int(input("¿Que temperatura hace hoy en ºc?"))
-if Temperatura > 17:
+if Temperatura > 20:
     print ("una chaqueta ligera esta bien")
 else:
     print ("Ponte abrigo hace frio")    
